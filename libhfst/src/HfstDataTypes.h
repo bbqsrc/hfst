c719318a583084039c99eb65b23dd9f602b36841
Brendan Molloy <brendan@necessary.nu>
2024-02-21 19:12:16 +0100

    More win32

--- libhfst/src/HfstDataTypes.h
+++ libhfst/src/HfstDataTypes.h
@@ -21,10 +21,10 @@
 #include <set>
 
 #ifdef _MSC_VER
-  #pragma warning(disable:4018 4244 4996 4099 4503 4800 4305 4355 4396 4715 4180 4244 4224 4267)
+  #include <io.h>
+  #pragma warning(disable:4005 4018 4244 4996 4099 4503 4800 4305 4355 4396 4715 4180 4244 4224 4267)
   #define HAVE_STD__TR1__HASH_LONG_LONG_UNSIGNED_ 1
   #define WIN32_LEAN_AND_MEAN
-  #include <io.h>
   #define strdup _strdup
   #define isatty _isatty
   #define fileno _fileno
